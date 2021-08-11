9ee129a839174139c4bf8717182d282068de7ba6
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-08-11 01:01:22 -0400

    all test passing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"]
+const eventName = "surprise"
+function writeCards(names, eventName) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards; 
+}
+
+function countDown(number) {
+     
+    while (number > 0) {console.log(number);
+        number -= 1;
+}
+    console.log(number);
+}
